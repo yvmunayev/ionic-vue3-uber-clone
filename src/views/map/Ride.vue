49db8b329a4328e44c9459d2abb19722d6ee0494
Yuri Victor Munayev <yvmunayev@gmail.com>
2021-12-14 19:03:14 -0500

    feat: add logic to Ride component

--- src/views/map/Ride.vue
+++ src/views/map/Ride.vue
@@ -11,38 +11,75 @@
 
         <ion-grid v-show="distance" class="ion-margin">
         <ion-list>
-            <ion-item v-for="item in mapRideData" :key="item.title">
-            <ion-thumbnail class="ride-thumbnail">
-                <ion-img class="ride-img" :src="item.image"></ion-img>
-            </ion-thumbnail>
-            <ion-label class="ride-label">
-                <h3>{{item.title}}</h3>
-                <p>{{ durationHHMM }}</p>
-            </ion-label>
-            <ion-label class="ride-price-label">
-                <ride-price :duration="duration" :multiplier="item.multiplier" ></ride-price>
-            </ion-label>
+            <ion-item
+                button
+                v-for="item in mapRideData"
+                :key="item.title"
+                @click="handleClick(item)"
+                :color="chosenOption && item.title === chosenOption.title ?  'secondary' : 'light' "
+            >
+                <ion-thumbnail class="ride-thumbnail">
+                    <ion-img class="ride-img" :src="item.image"></ion-img>
+                </ion-thumbnail>
+                <ion-label class="ride-label">
+                    <h3>{{item.title}}</h3>
+                    <p>{{ durationHHMM }}</p>
+                </ion-label>
+                <ion-label class="ride-price-label">
+                    <ride-price :duration="duration" :multiplier="item.multiplier" ></ride-price>
+                </ion-label>
             </ion-item>
         </ion-list>
+        <br/>
+        <ion-button expand="block" size="large" color="danger" :disabled="!chosenOption" @click="handleChoose">Choose</ion-button>
         </ion-grid>
     </div>
 </template>
 
 <script setup>
-    import { computed} from "@vue/runtime-core";
+    import { computed, ref} from "@vue/runtime-core";
     import { useStore } from "vuex";
-    import { IonGrid, IonItem, IonLabel, IonList, IonThumbnail, IonImg, IonButton, IonTitle } from '@ionic/vue';
+    import { IonGrid, IonItem, IonLabel, IonList, IonThumbnail, IonImg, IonButton, IonTitle, toastController } from '@ionic/vue';
     import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
     import RidePrice from '../../components/RidePrice.vue';
     import toHHMMSS from '../../helpers/toHHMMSS';
     import mapRideData from '../../data/mapRideData.json'
-    
+
+    const chosenOption = ref();
     const store = useStore();
     const distance = computed(
         () => store.state.travelTimeInfo && parseInt(store.state.travelTimeInfo.routes[0].distance / 1609, 10)
     );
     const duration = computed(() => store.state.travelTimeInfo && store.state.travelTimeInfo.routes[0].duration);
     const durationHHMM = computed(() => duration.value && toHHMMSS(duration.value));
+
+    const handleClick = (item) => {
+        chosenOption.value = item;
+    }
+
+    const handleChoose = async () => {
+        const toast = await toastController.create({
+            message: `You have chosen an ${chosenOption.value.title} for your trip.`,
+            duration: 2000,
+            color: 'success',
+            buttons: [{
+                role: 'cancel',
+                text: 'X',
+            }],       
+        });
+
+        await toast.present();
+
+        const { role } = await toast.onDidDismiss();
+        if (role == 'cancel') {
+            const cancelToast = await toastController.create({
+                message: `Your ${chosenOption.value.title} ride has been canceled.` ,
+                color: 'danger',
+                duration: 2000,      
+            });
+            await cancelToast.present();
+        }
+    }
 
 </script>
 
